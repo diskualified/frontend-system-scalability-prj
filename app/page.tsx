06c24384d4a32d8a4fe31c873614a924ea48a6f5
David Qian <david@sharewellnow.com>
2026-04-03 16:37:13 -0700

    use buffer interval to lower amt of re-renders

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 
 interface DataItem {
   id: number;
@@ -10,25 +10,33 @@ interface DataItem {
 
 export default function Home() {
   const [data, setData] = useState<Record<number, DataItem>>({});
+  const bufferRef = useRef<Record<number, DataItem>>({});
 
   useEffect(() => {
     const ws = new WebSocket('ws://localhost:8080');
 
     ws.onmessage = (event) => {
       const updates: DataItem[] = JSON.parse(event.data);
-
-      setData((prev) => {
-        const newData = { ...prev };
-        updates.forEach((u) => {
-          newData[u.id] = u;
-        });
-
-        return newData;
+      // reformat updates data into map
+      updates.forEach((u) => {
+        bufferRef.current[u.id] = u;
       });
     };
 
+    // re-render every 500ms with latest data in buffer
+    const interval = setInterval(() => {
+      const pending = bufferRef.current;
+      if (Object.keys(pending).length === 0) {
+        return;
+      }
+      // pending overwrites previous data with same id
+      setData((prev) => ({ ...prev, ...pending }));
+      bufferRef.current = {};
+    }, 500);
+
     return () => {
       ws.close();
+      clearInterval(interval);
     };
   }, []);
 
@@ -38,7 +46,8 @@ export default function Home() {
       <div className='h-full overflow-y-auto'>
         {Object.values(data).map((item) => (
           <div key={item.id}>
-            {item.id}: {item.value.toFixed(2)}
+            {item.id}: {item.value.toFixed(2)} (at{' '}
+            {new Date(item.timestamp).toLocaleTimeString()})
           </div>
         ))}
       </div>
